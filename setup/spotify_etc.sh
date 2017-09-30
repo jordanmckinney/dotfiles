4534b7b0abd0ad39f9f9113e1272c9f780b0a576
Jordan McKinney <jordanmmck@gmail.com>
2017-09-29 19:46:19 -0700

    zcash mining

--- setup/spotify_etc.sh
+++ setup/spotify_etc.sh
@@ -15,5 +15,14 @@ echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sou
 sudo apt-get update
 sudo apt-get install sublime-text
 
+# keybase
+curl -O https://prerelease.keybase.io/keybase_amd64.deb
+# if you see an error about missing `libappindicator1`
+# from the next command, you can ignore it, as the
+# subsequent command corrects it
+sudo dpkg -i keybase_amd64.deb
+sudo apt-get install -f
+run_keybase
+
 # krita
 sudo apt install krita
