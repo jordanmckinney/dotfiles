db9272ddf65f2bc2ad7c56f2dd0fc41fe7ebfce2
Jordan McKinney <jordanmmck@gmail.com>
2018-08-05 11:09:43 -0700

    more tweaks

--- setup/setup.sh
+++ setup/setup.sh
@@ -13,8 +13,8 @@
 
 sudo apt install curl tree htop tmux zsh xsel silversearcher-ag trimage git
 
-git config --global user.email "jordanmmck@gmail.com"
-git config --global user.name "Jordan McKinney"
+git config --global user.email "jordanmmck@gmail.com" && \
+git config --global user.name "Jordan McKinney" && \
 git config --global push.default simple
 
 ssh-keygen -t rsa -b 4096 -C "jordanmmck@gmail.com"
@@ -29,17 +29,13 @@ pip3 install virtualenvwrapper
 pip install autoenv
 pip3 install autoenv
 
-rmdir ~/Music
-rmdir ~/Public
-rmdir ~/Videos
+rmdir ~/Music && \
+rmdir ~/Public && \
+rmdir ~/Videos && \
 rmdir ~/Templates
-rmdir ~/Pictures
-
-mkdir ~/Development
+# mkdir ~/Development
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 
 sudo apt update
 sudo apt install libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev
@@ -52,6 +48,8 @@ echo "source ~/Development/DotFiles/tmux.conf" >> ~/.tmux.conf
 cp ~/Development/DotFiles/.zprofile ~/.zprofile
 
 curl -sL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash -
+
+# restart
 rbenv install 2.5.1
 rbenv global 2.5.1
 
@@ -61,6 +59,7 @@ mkdir ~/.bin
 cp tmuxinator/completion/tmuxinator.zsh ~/.bin
 rm -rf tmuxinator
 
+# NEOVIM
 sudo apt-add-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install neovim
@@ -70,22 +69,31 @@ pip3 install neovim
 pip2 install --upgrade neovim
 pip3 install --upgrade neovim
 
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+nvm install node
+npm install npm -g
+npm install -g neovim
+
+gem install neovim
+
+git clone https://github.com/powerline/fonts ~/.local/share/fonts
+
+mkdir ~/.config/nvim
+ln -s ~/Development/DotFiles/init.vim ~/.config/nvim/ && \
+ln -s ~/Development/DotFiles/vim/after ~/.config/nvim/ && \
+ln -s ~/Development/DotFiles/vim/colors ~/.config/nvim/ && \
+ln -s ~/Development/DotFiles/vim/syntax ~/.config/nvim/
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cd ~/.config/nvim
+git clone https://github.com/Shougo/deoplete.nvim.git
+
 # ```
 # :PlugInstall
 # :PlugUpdate
 # :UpdateRemotePlugins
 # :checkhealth
 # ```
-
-# nvm, npm, node
-nvm install node
-sudo ln -s /usr/bin/nodejs /usr/bin/node
-npm install npm -g
-npm install -g neovim
-
-git clone https://github.com/powerline/fonts ~/.local/share/fonts
-# Set font to Source Code Pro for Powerline in system 
-# Also set other fonts
 
 # yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
@@ -97,15 +105,9 @@ if [ -n "$VIRTUAL_ENV" ] ; then
     deactivate
 fi
 
-# add new ssh key to github
-# go into disks, make harddrive sleep
-# Set terminal to theme colours, transparency
-# set lock screen
-# search "dock" to set dock to hide
-
-sudo add-apt-repository ppa:daniruiz/flat-remix
-sudo apt-get update
-sudo apt-get install flat-remix-gnome
+#sudo add-apt-repository ppa:daniruiz/flat-remix
+#sudo apt-get update
+#sudo apt-get install flat-remix-gnome
 sudo apt install gnome-shell-extensions
 
 # shell
@@ -125,3 +127,11 @@ cd /tmp && rm -rf flat-remix-gtk &&
 git clone https://github.com/daniruiz/flat-remix-gtk &&
 mkdir -p ~/.themes && cp -r flat-remix-gtk/Flat-Remix-GTK* ~/.themes/ &&
 gsettings set org.gnome.desktop.interface gtk-theme "Flat-Remix-GTK"
+
+# Set font to Source Code Pro for Powerline in system 
+# Also set other fonts
+# add new ssh key to github
+# go into disks, make harddrive sleep
+# Set terminal to theme colours, transparency
+# set lock screen
+# search "dock" to set dock to hide
