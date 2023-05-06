1ddb6d96978e948e72eb4e0c3285a8fcc00231e8
Jordan McKinney <jordanmmck@gmail.com>
2023-05-06 10:28:27 -0700

    add sync script

---
+++ sync.sh
@@ -0,0 +1,7 @@
+rsync -av --progress --delete --ignore-errors --exclude='.Trashes' --exclude='.Spotlight-V100' --exclude='.TemporaryItems' --exclude='.DocumentRevisions-V100' --exclude='.DS_Store' --exclude='.fseventsd' /Users/jordan/Downloads /Volumes/SSD/
+
+rsync -av --progress --delete --ignore-errors --exclude='.Trashes' --exclude='.Spotlight-V100' --exclude='.TemporaryItems' --exclude='.DocumentRevisions-V100' --exclude='.DS_Store' --exclude='.fseventsd' /Users/jordan/Library/Mobile\ Documents/com\~apple\~CloudDocs/JORDANMMCK /Volumes/SSD/Documents
+
+rsync -av --progress --delete --ignore-errors --exclude='.Trashes' --exclude='.Spotlight-V100' --exclude='.TemporaryItems' --exclude='.DocumentRevisions-V100' --exclude='.DS_Store' --exclude='.fseventsd' /Users/jordan/Documents/journal /Volumes/SSD/Documents
+
+rsync -av --progress --delete --ignore-errors --exclude='.Trashes' --exclude='.Spotlight-V100' --exclude='.TemporaryItems' --exclude='.DocumentRevisions-V100' --exclude='.DS_Store' --exclude='.fseventsd' /Volumes/SSD /Volumes/HDD
